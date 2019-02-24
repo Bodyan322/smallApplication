f2441284c69ba13e559153efbe39f634687b864b
Bodyan322 <bogdan3206@gmail.com>
2019-02-24 16:36:12 +0200

    addAngular, add module to index.js

--- dest/js/index.js
+++ dest/js/index.js
@@ -1,7 +1,3 @@
 
-let app = angular.module('app', []);
-app.controller('haha', function($scope) {
-  console.log('haha');
-  $scope.hello = 'kuku';
-});
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImpzL3NlcnZpY2UuanMiLCJqcy9jb250cm9sbGVyLmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBO0FDQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQSIsImZpbGUiOiJpbmRleC5qcyIsInNvdXJjZXNDb250ZW50IjpbIiIsImxldCBhcHAgPSBhbmd1bGFyLm1vZHVsZSgnYXBwJywgW10pO1xyXG5hcHAuY29udHJvbGxlcignaGFoYScsIGZ1bmN0aW9uKCRzY29wZSkge1xyXG4gIGNvbnNvbGUubG9nKCdoYWhhJyk7XHJcbiAgJHNjb3BlLmhlbGxvID0gJ2t1a3UnO1xyXG59KTsiXX0=
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImpzL3NlcnZpY2UuanMiLCJqcy9jb250cm9sbGVyLmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBO0FDQUEiLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VzQ29udGVudCI6WyIiLCIiXX0=
--- src/js/controller.js
+++
@@ -1,5 +0,0 @@
-let app = angular.module('app', []);
-app.controller('haha', function($scope) {
-  console.log('haha');
-  $scope.hello = 'kuku';
-});
