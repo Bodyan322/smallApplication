42857823fd6030314224392d4a6bc199e75d83aa
Bodyan322 <bogdan3206@gmail.com>
2019-02-25 15:26:23 +0200

    addAngular, add function for changable button add convertable finction add filter method to selects

--- src/js/controller.js
+++ src/js/controller.js
@@ -7,5 +7,22 @@
     this.currency = currServ.loadCache();
     this.currencyGiven = { ccy: 'USD', buy: '26.85000', sale: '27.15000' };
     this.currencyReceiven = { ccy: 'EUR', buy: '30.30000', sale: '30.80000' };
+
+    this.changeValues = () => {
+      [this.countValue, this.costValue] = [this.costValue, this.countVal];
+      [this.currencyGiven, this.currencyReceiven] = [this.currencyReceiven, this.currencyGiven];
+    };
+
+    this.convertValue = () => {
+      let res = 0;
+      res = currServ.convertTo(this.countValue, this.currencyGiven.buy);
+      this.costValue = currServ.convertFrom(res, this.currencyReceiven.sale);
+    };
   }]);
+
+  app.filter('filterCurr', function() {
+    return function(array, filterValue) {
+      return array.filter(item => item.ccy !== filterValue);
+    };
+  });
 }());
