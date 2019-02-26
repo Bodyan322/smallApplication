15844dace04453e6769e97a650faa5274377cbd9
Bodyan322 <bogdan3206@gmail.com>
2019-02-26 10:38:59 +0200

    refactor, add watchers to controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,28 +1,27 @@
 /* global app */
 (function() {
-  app.controller('controllServ', ['currServ', function(currServ) {
+  app.controller('controllServ', ['$scope', 'currServ', 'constantValues', function($scope, constantValues, currServ) {
     this.countValue = null;
     this.costValue = null;
-    this.activeTab = true;
     this.currency = currServ.loadCache();
-    this.currencyGiven = { ccy: 'USD', buy: '26.85000', sale: '27.15000' };
-    this.currencyReceiven = { ccy: 'EUR', buy: '30.30000', sale: '30.80000' };
+    this.currencyGiven = constantValues.currency[0];
+    this.currencyReceiven = constantValues.currency[1];
 
     this.changeValues = () => {
-      [this.countValue, this.costValue] = [this.costValue, this.countVal];
+      [this.countValue, this.costValue] = [this.costValue, this.countValue];
       [this.currencyGiven, this.currencyReceiven] = [this.currencyReceiven, this.currencyGiven];
     };
 
     this.convertValue = () => {
-      let res = 0;
-      res = currServ.convertTo(this.countValue, this.currencyGiven.buy);
+      const res = currServ.convertTo(this.countValue, this.currencyGiven.buy);
       this.costValue = currServ.convertFrom(res, this.currencyReceiven.sale);
     };
-  }]);
 
-  app.filter('filterCurr', function() {
-    return function(array, filterValue) {
-      return array.filter(item => item.ccy !== filterValue);
-    };
-  });
+    $scope.$watch('ng.currencyGiven', () => {
+      this.convertValue();
+    });
+    $scope.$watch('ng.currencyReceiven', () => {
+      this.convertValue();
+    });
+  }]);
 }());
